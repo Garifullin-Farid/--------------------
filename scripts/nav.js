4680acbbff869c01b3436aef761f99d3d7212bcd
Garifullin-Farid <95081032+Garifullin-Farid@users.noreply.github.com>
2024-04-16 22:05:21 +0400

    11

---
+++ scripts/nav.js
@@ -0,0 +1,9 @@
+/* Установите ширину боковой панели на 250 пикселей (показать его) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "280px";
+  }
+  
+  /* Установите ширину боковой панели в 0 (скройте ее) */
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
